c589073cf185204f1412e2fb40c6f5f6e194107a
luizfm <luiz.ado@hotmail.com>
2022-05-06 00:20:50 -0300

    feat: Start meta heuristic algorithm

--- meta-heuristic/index.js
+++ meta-heuristic/index.js
@@ -1,1 +1,48 @@
-// Empty at the moment
+const createSolution = () => {}
+
+const cost = () => {}
+
+const crossover = () => {}
+
+class GeneticSudokuSolver {
+  constructor(board) {
+    this.populationSize = 100;
+    this.mutationProbability = 0.2;
+    this.elitism = 0.2;
+    this.generationNumbers = 500;
+    this.board = board;
+  }
+
+  execute() {
+    let population = []
+    for(let i = 0; i < this.populationSize; i++) {
+      population.add(createSolution())
+    }
+
+    const elitismNumber = this.elitism * this.populationSize
+
+    for(let i = 0; i < this.generationNumbers; i++) {
+      population.sort((a, b) => {
+        return cost(a) - cost(b)
+      })
+
+      population = population.splice(0, elitismNumber);
+      while(subList.length < this.populationSize) {
+          const nextInt = Math.floor(Math.random() * elitismNumber)
+          population.add(mutation(population[nextInt]))
+        // if(new Math.random() < this.mutationProbability) {
+        //   const nextInt = Math.floor(Math.random() * elitismNumber)
+        //   population.add(mutation(population[nextInt]))
+        // } else {
+        //   const c1 = Math.floor(Math.random() * elitismNumber)
+        //   const c2 = Math.floor(Math.random() * elitismNumber)
+        //   population.add(crossover(population[c1], population[c2]))
+        // }
+      }
+    }
+    population.sort((a, b) => {
+      return a - b
+    })
+    return population[0]
+  }
+}
